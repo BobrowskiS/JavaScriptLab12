03c413813048f00be482b5dc0c220bf9bc5e0d3f
Stanislav Bobrowski <32281057+BobrowskiS@users.noreply.github.com>
2018-11-21 19:32:30 +0300

    Update script.php

--- script.php
+++ script.php
@@ -1,5 +1,8 @@
 <?php 
-echo 'Ответ на вопрос';
+echo 'Ответ на вопрос: гугл выдает следующее - "Origin null is the local file system, so that suggests that 
+you're loading the HTML page that does the load call via a file:/// URL (e.g., just double-clicking it in a local file browser or similar). 
+Different browsers take different approaches to applying the Same Origin Policy to local files." 
+Пруф/линк: https://stackoverflow.com/questions/8456538/origin-null-is-not-allowed-by-access-control-allow-origin '
 ?>
 
 
